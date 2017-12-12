970faf0718bc8ff09bb8ad4ac978ea1b18ea2e42
Advay Mengle (at Holvonix LLC) <advay-github@holvonix.com>
2017-12-11 20:46:26 -0800

    Pushver script

---
+++ scripts/pushver.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -euv
+VERSION=$1
+git commit -m $VERSION
+git tag -s -m $VERSION $VERSION
+git push public --tags || echo No public repo.
+git push --tags
+
+git push public || echo No public repo.
+git push
